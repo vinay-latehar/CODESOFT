f82a4f47749eab9d79d20c3a5789e42079ce220d
Vinay Kumar Prajapati <132884994+vinay-latehar@users.noreply.github.com>
2024-07-10 17:46:37 +0530

    Add files via upload

---
+++ RandomNumber.java
@@ -0,0 +1,16 @@
+/*  TASK 1 : 1. Generate a random number within a specified range, such as 1 to 100. */
+
+public class RandomNumber{
+
+        	public static void main(String[] args)
+		 {
+              
+               			double random = Math.random();
+              
+               			int range = 100;
+              
+              			System.out.println("Random number between 1 to 100 : "+(int)(random*range));
+              
+     		  }
+
+}
